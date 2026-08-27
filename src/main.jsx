f5379164673b175142446c99f435429260a41cf8
Eesha Bharathy J <eeshajeyaraj@gmail.com>
2026-08-27 22:14:53 +0530

    Load global theme palettes before app render

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/theme-palettes.css'
 import './styles/room-cards.css'
 import './styles/studyverse-ui.css'
 import './styles/chat-ui.css'
